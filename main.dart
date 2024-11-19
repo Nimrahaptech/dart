63f8c7fdbe38a84c608a93d22d559dff808abf89
Nimrahaptech <136672693+Nimrahaptech@users.noreply.github.com>
2024-11-19 12:44:07 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,88 @@
+// 
+// ignore_for_file: unused_import
+
+import './makeupcity.dart';
+import './nyx.dart';
+
+// class customer{
+//    late String name;
+//    late String item1;
+//    int price = int.parse(item1);
+//    late String item2;
+//    late double price;
+   
+//    late double total; 
+
+//    customer (String name, String item1 , String item2,  double total){
+//      this.name=  name;
+//      this.item1 = item1;
+//      this.item2 = item2;
+
+//      this.total = this.item1 + this.item2;
+
+  
+
+
+//       void display(){
+//         print('Name: $name');
+//         print('Item: $item1');
+//         print('Item: $item2');
+       
+       
+//         print('Total: $total');
+//       }
+//       void show(int quantity){
+//         print('Hi, $name your  total is $total * $quantity ');
+
+
+//       }
+
+      
+//    }
+   
+//      void display() {
+//        print('Name: $name');
+//        print('Item: $item1');
+//        print('Item: $item2');
+//        print('Total: $total');
+//      }
+     
+//        void show(int i) {
+//          print('Hi, $name your  total is $total * $i ');
+//        }
+     
+     
+     
+      
+
+
+// }
+// void main(){
+//   customer cust1 = new customer('John Doe', 'Shirt', 'Pants', 100.0);
+//   cust1.display();
+//   cust1.show(3);
+// }
+void main(){
+  // print(
+  //   'Name: John Doe\n'
+  //   'Item: Shirt\n'
+  //   'Item: Pants\n'
+  //   'Total: 100.0\n'
+  //   'Hi, John Doe your  total is 100.0 * 3 = 300.0'
+  // );
+   
+ 
+   
+  // Create an instance of Customer  
+  // customer cust1 = new customer('John Doe', '123@',1234);
+  //     var firstorder;
+  //     firstorder.discountcode;
+
+
+ 
+
+}
+
+
+
+
