acb7334dcc41300b591cba01c8108f0568eee742
Nimrahaptech <136672693+Nimrahaptech@users.noreply.github.com>
2024-11-19 12:42:39 +0500

    Add files via upload

---
+++ display.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: unused_import
+
+// import 'display.dart';
+
+// import 'nyx.dart';
+// import './makeupcity.dart';
+
+// class makeup implements hudabeauty,nyx{
+//   @override
+ 
+//   void fullcoverage(){
+//     print('our new launch');
+  
+//   }
+
+//   @override
+//   void matfinish() {
+//     print('huda mat finish');
+   
+//   }
+  
+//   @override
+//   void gloss(String shades) {
+//     print('huda shades: $shades');
+//   }
+  
+//   @override
+//   void waterproof() {
+//     print('huda waterproof');
+//   }
+//  void main(){
+//    makeup makeup = new makeup();
+//    makeup.fullcoverage();
+//    makeup.matfinish();
+//    makeup.gloss('huda shades');
+//    makeup.waterproof();
+//  }
+
+ 
+// }
+
